683d3d3342f535af88bb1bba11fce6232ce9ab0d
SuperCode <supercode@SuperCodes-MacBook-Air.local>
2020-04-07 01:59:30 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+function DieseZahlMultiplizieren(){
+    const dieZahl=document.getElementById("multiplizieren").value
+    const dieMultiplazierung= 2 * dieZahl;
+    document.getElementById("ergbniss").innerText=dieMultiplazierung;
+
+}
